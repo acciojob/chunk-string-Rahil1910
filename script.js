9654b7102a1b8ff3604cf67f9161320c7701f650
Rahil1910 <rahilrafique1234@gmail.com>
2024-12-02 11:41:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 function stringChop(str, size) {
   // your code here
-}
+	let cnt=0,ans="";
+	let arr=[];
+	if(str==""){
+		return arr;
+	}
+	if(str.length<=size){
+		arr.push(str);
+		return arr;
+	}
+	for(let i=0;i<str.length;i++){
+		ans+=str[i];
+		cnt++;
+		if(cnt===size){
+			arr.push(ans);
+			ans="";
+			cnt=0;
+		}
+	}
+	if (ans !== "") {
+	    arr.push(ans);
+	}
+	return arr;
+}  
 
 // Do not change the code below
 const str = prompt("Enter String.");
